c8f123fb87c509ea401ae422f306a919b8ed73e3
awaisrajput99 <162193480+awaisrajput99@users.noreply.github.com>
2024-03-07 01:18:54 +0500

    Initial commit

---
+++ Basics C#/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Basic input and Output
+            Console.WriteLine("Hellow World");
+            Console.WriteLine("My First C# Program");
+            Console.ReadLine();
+
+            // Data Types in C#
+            int a = 10;     // Integer-->4bytes
+            long b = 54;    // Long--> 8bytes
+            float c = 35;   // floating points number-->4bytes
+            double d = 24.32; //  Double--8bytes
+            char E = 'a';    // For Characters-->2bytes
+            String f = "Hope for the best"; //For String Values-->2bytes per chracter
+            bool isGreat = true;   //For Boolean values true or false
+
+            Console.WriteLine("It was a Great Learning");
+            Console.ReadLine();
+
+        }
+    }
+}
